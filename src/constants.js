b21a91d4dc6ea71a115d96a9a6b718f6e82141a0
Khairul-2000 <kahirulislam135@gmail.com>
2025-01-26 23:50:57 +0600

    c added

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,7 @@ export const LANGUAGE_VERSIONS = {
   java: "15.0.2",
   csharp: "6.12.0",
   php: "8.2.3",
+  c: "10.2.0",
 };
 
 export const CODE_SNIPPETS = {
@@ -12,6 +13,7 @@ export const CODE_SNIPPETS = {
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
   python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  c: `\n#include<stdio.h>\n\nint main(){\nprintf("Hello World in C");\nreturn 0;\n}\n`,
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Alex';\necho $name;\n",
